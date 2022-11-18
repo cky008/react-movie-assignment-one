a5c4630cde2be95e51eb023c74b4ae85118eaf64
Kaiyu Chen <greatcky83@gmail.com>
2022-11-18 02:54:42 +0000

    Fix Review Page.

--- src/components/reviewForm/index.js
+++ src/components/reviewForm/index.js
@@ -69,7 +69,7 @@ const styles = {
 
     const handleSnackClose = (event) => {
         setOpen(false);
-        navigate("/movies/favorites");
+        navigate("/movies/favorites/page1");
       };
   
     const defaultValues = {
